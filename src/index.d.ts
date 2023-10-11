ea9b9e1448bd25c442a146222d23b5d14fbfbe95
Connor McLaughlin <connor1211.mcl@gmail.com>
2023-10-11 21:58:31 +0100

    Update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,14 @@
-/**
- * @param {string} input
- * @returns {State}
- */
+export function htmlLocationToPugLocation(index: number, state: State): number;
+export function pugLocationToHtmlLocation(index: number, state: State): number;
 export function parse(input: string): State;
 export type Range = {
-    htmlEnd: number;
-    htmlStart: number;
-    pugEnd: number;
-    pugStart: number;
+  htmlEnd: number;
+  htmlStart: number;
+  pugEnd: number;
+  pugStart: number;
 };
 export type State = {
-    htmlText: string;
-    pugText: string;
-    ranges: Range[];
+  htmlText: string;
+  pugText: string;
+  ranges: Range[];
 };
-//# sourceMappingURL=index.d.ts.map
--- types/src/index.d.ts
+++ types/src/index.d.ts
@@ -1,20 +1,5 @@
-/**
- * @param {number} index
- * @param {State} state
- * @returns {number}
- */
-export function htmlLocationToPugLocation(index, state): number;
-/**
- * @param {number} index
- * @param {State} state
- * @returns {number}
- */
-export function pugLocationToHtmlLocation(index, state): number;
-export function parse(input: string): State;
-/**
- * @param {string} input
- * @returns {State}
- */
+export function htmlLocationToPugLocation(index: number, state: State): number;
+export function pugLocationToHtmlLocation(index: number, state: State): number;
 export function parse(input: string): State;
 export type Range = {
     htmlEnd: number;
